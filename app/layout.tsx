644c745c7ed8b94855f228eec85782f1658a1cdf
Anjan-IT <sangeetasystems.cs@gmail.com>
2024-10-20 14:33:04 +0530

    google ads added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import React from "react";
+import Script from "next/script"; // Import next/script
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,7 +31,58 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16456011279"
+          strategy="afterInteractive" // Loads the script after user interaction
+        />
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16456011279');
+          `}
+        </Script>
       </body>
     </html>
   );
 }
+
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+
+// import "./globals.css";
+// import { ThemeProvider } from "./provider";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "SANGEETA SYSTEMS",
+//   description: "IT Solutions",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en" suppressHydrationWarning>
+//       <head>
+//         <link rel="icon" href="/SANGEETA.png" sizes="any" />
+//       </head>
+//       <body className={inter.className}>
+//         <ThemeProvider
+//           attribute="class"
+//           defaultTheme="light"
+//           enableSystem
+//           disableTransitionOnChange
+//         >
+//           {children}
+//         </ThemeProvider>
+//       </body>
+//     </html>
+//   );
+// }
